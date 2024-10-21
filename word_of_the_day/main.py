2d2b8f81b7afebd0f66d15429baa7224f3e4c749
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-10-21 21:10:46 +0200

    incomplete

--- word_of_the_day/main.py
+++ word_of_the_day/main.py
@@ -1,6 +1,8 @@
 import smtplib
+import random
 
-
+with open('qoutes.text' 'r', errors='ignore') as f:
+        qoutes = f.read()
 def get_qoute():
     """
     TODO
@@ -8,7 +10,9 @@ def get_qoute():
     and returns a randomly selected qoute from the 
     qoutes.txt file.
     """
-
+    qoutes_lines = qoutes.splitlines()
+    rand_qoute = random.randrange(0, len(qoutes_lines))
+    return qoutes_lines[rand_qoute]
 
 def send_email(message):
     """
@@ -17,6 +21,26 @@ def send_email(message):
     an email to your self using 
     smtplib and return 'success ' or 'fail' if sending email failed
     """
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+
+    mail.ehlo()
+
+    mail.starttls()
+
+    sender = 'scelonkululeko2@gmail.com'
+    password = 'blackrage'
+
+    mail.login(sender, password)
+
+    recipient = 'scelonkululeeko2@gmail.com'
+    subject = 'Email to mt self'
+
+    header = f'To: {recipient}\nFrom: {sender}\nSubject: {subject}\n'
+    message = header + message
+
+    mail.quit()
+    
+
 
 def main():
     """
