d353864f5a90890b58b6fbacad3f2bc4d8a51726
kyle <kkndlovu9@gmail.com>
2024-10-21 18:27:36 +0200

    added another proj

---
+++ word_of_the_day/main.py
@@ -0,0 +1,25 @@
+import smtplib
+
+
+def get_qoute():
+    """
+    TODO
+    write a function that reads 'qoutes.txt'
+    and returns a randomly selected qoute from the 
+    qoutes.txt file.
+    """
+
+
+def send_email(message):
+    """
+    TODO
+    write a function that sends
+    an email to your self using 
+    smtplib and return 'success ' or 'fail' if sending email failed
+    """
+
+def main():
+    """
+    TODO
+    write logic to use the two functions here
+    """
