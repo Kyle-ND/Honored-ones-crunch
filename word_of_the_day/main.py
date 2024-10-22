3e904a8efe51de38632ad6d6df0298cc37c118b5
Khensani Chauke <khchaukjhb024@student.wethinkcode.co.za>
2024-10-22 17:13:06 +0200

    crunchy

--- word_of_the_day/main.py
+++ word_of_the_day/main.py
@@ -1,14 +1,16 @@
 import smtplib
 
 
-def get_qoute():
+def get_qoute(quotes_txt):
     """
     TODO
     write a function that reads 'qoutes.txt'
     and returns a randomly selected qoute from the 
     qoutes.txt file.
     """
-
+with open(quotes_txt, 'r') as file:
+        quotes = file.readlines()
+        print(random.choice(quotes).strip())
 
 def send_email(message):
     """
@@ -17,7 +19,8 @@ def send_email(message):
     an email to your self using 
     smtplib and return 'success ' or 'fail' if sending email failed
     """
-
+    sender = "Khensaneo@icloud.com"
+    receiver = "Khensaneo@icloud.com"
 def main():
     """
     TODO
