33801e1a0d6c4b5be5dfe42efa403e4e7343b222
Onalerona <onzwanejhb024@student.wethinkcode.co.za>
2024-10-22 14:20:38 +0200

    Only have quotes left

--- word_of_the_day/main.py
+++ word_of_the_day/main.py
@@ -1,5 +1,5 @@
 import smtplib
-
+import random
 
 def get_qoute():
     """
@@ -8,6 +8,17 @@ def get_qoute():
     and returns a randomly selected qoute from the 
     qoutes.txt file.
     """
+    with open('word_of_the_day/qoutes.txt', 'r') as quotes_file:
+        file_reader = quotes_file.readlines()
+
+        rand_index = random.randrange(0,len(file_reader) -1)
+        random_quote = file_reader[rand_index]
+        print(random_quote)
+        return random_quote
+get_qoute()
+
+
+
 
 
 def send_email(message):
