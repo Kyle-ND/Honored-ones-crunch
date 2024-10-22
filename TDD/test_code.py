5ac93abfe4a7cb879f147c90c31c8049a9d5d605
khomotso <kkgothamama@gmail.com>
2024-10-22 20:02:12 +0200

    Project

---
+++ TDD/test_code.py
@@ -0,0 +1,13 @@
+def add(a,b):
+    return a + b
+ 
+def subtract(a,b):
+    return a - b
+
+def multply(a,b):
+    return a * b
+
+def divide(a,b):
+    if b == 0:
+        return "invaild"
+    return a/b
--- contact_book/main.py
+++ contact_book/main.py
@@ -1,2 +1,15 @@
 #use python dictionaries to create a function that prompts a user to enter their name and number and 
-#display what the contact book looks like at the moment 
+#display what the contact book looks like at the moment
+ 
+def Add_contacts():
+
+    contact_book = {}
+
+    name = input("Enter Name: ")
+    number = input("Enter Number: ")
+
+    contact_book[name] = number
+    print(contact_book)
+
+Add_contacts()
+
