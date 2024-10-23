25c1ff7ecaecff50e31854f3bc70f94fc15254f0
wtc <wtc@pop-os.localdomain>
2024-10-23 14:41:49 +0200

    4th

--- word_of_the_day/main.py
+++ word_of_the_day/main.py
@@ -1,5 +1,4 @@
 import smtplib
-
 import random
 
 
@@ -9,14 +8,15 @@ write a function that reads 'qoutes.txt'
 and returns a randomly selected qoute from the 
 qoutes.txt file.
 """
-def choose_random_line(file_path): 
-    with open(file_path, 'r') as file: 
-        lines = file.readlines()  
-    return random.choice(lines).strip()  
+def choose_random_line(): 
 
-file_path = 'qoutes.txt'  
-random_line = choose_random_line(file_path) 
-print(random_line) 
+    try:
+        with open("word_of_the_day/qoutes.txt", 'r') as file: 
+            lines = file.readlines()  
+        return random.choice(lines).strip()  
+    except FileNotFoundError:
+        print("This file does not exist")
+
 
 
 
@@ -26,17 +26,28 @@ write a function that sends
 an email to your self using 
 smtplib and return 'success ' or 'fail' if sending email failed
 """
-def send_email(message):
+def send_email(receiver_email):
+    message = choose_random_line()
     s = smtplib.SMTP('madeupemail@gmail.com', 587)
     s.starttls()
     s.login("madeupemail@gmail.com", "Whateverthepasswordis30#")
     message = "Message you need to send"
-    s.sendmail("madeupemail@gmail.com", message)
-    s.quit()
-
+    s.sendmail(receiver_email, message)
+    return True
+    
     """
     TODO
     write logic to use the two functions here
     """
 
 def main():
+    receiver_email = input("Enter email: ")
+    if send_email(receiver_email):
+        print("Email sent")
+    
+
+   
+
+main()
+
+
