96ad103c3432649f241dbea2b130156d1e3df774
wtc <wtc@pop-os.localdomain>
2024-10-23 08:54:59 +0200

    2nd

--- word_of_the_day/main.py
+++ word_of_the_day/main.py
@@ -1,30 +1,42 @@
 import smtplib
 
 import random
-def get_qoute(qoutes.txt):
-    """
-    TODO
-    write a function that reads 'qoutes.txt'
-    and returns a randomly selected qoute from the 
-    qoutes.txt file.
-    """
-with open(qoutes.txt, 'r') as file: 
-	lines = file.readlines() 
-	random_line = random.choice(lines) 
-    return random_line 
-    
 
 
+"""
+TODO
+write a function that reads 'qoutes.txt'
+and returns a randomly selected qoute from the 
+qoutes.txt file.
+"""
+def choose_random_line(file_path): 
+    with open(file_path, 'r') as file: 
+        lines = file.readlines()  
+    return random.choice(lines).strip()  
+
+file_path = 'qoutes.txt'  
+random_line = choose_random_line(file_path) 
+print(random_line) 
+
+
+
+"""
+TODO
+write a function that sends
+an email to your self using 
+smtplib and return 'success ' or 'fail' if sending email failed
+"""
 def send_email(message):
-    """
-    TODO
-    write a function that sends
-    an email to your self using 
-    smtplib and return 'success ' or 'fail' if sending email failed
-    """
+    s = smtplib.SMTP('madeupemail@gmail.com', 587)
+    s.starttls()
+    s.login("madeupemail@gmail.com", "Whateverthepasswordis30#")
+    message = "Message you need to send"
+    s.sendmail("madeupemail@gmail.com", message)
+    s.quit()
 
-def main():
     """
     TODO
     write logic to use the two functions here
     """
+
+def main():
