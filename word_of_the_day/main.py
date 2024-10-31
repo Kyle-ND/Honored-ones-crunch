9f0f299c04aedd5496ca8bed470f76c6072229f4
NkululekoNtuli <scelonkululeko2@gmail.com>
2024-10-31 09:12:38 +0200

    reupload of word of the day(must revisite to impliment .gitignore.)

--- word_of_the_day/main.py
+++ word_of_the_day/main.py
@@ -1,5 +1,5 @@
 import smtplib
-
+import random
 
 def get_qoute():
     """
@@ -8,7 +8,12 @@ def get_qoute():
     and returns a randomly selected qoute from the 
     qoutes.txt file.
     """
+    with open('word_of_the_day/qoutes.txt', 'r', errors='ignore') as f:
+        qoutes = f.read()
 
+    qoutes_lines = qoutes.splitlines()
+    rand_qoute = random.randrange(0, len(qoutes_lines))
+    return qoutes_lines[rand_qoute]
 
 def send_email(message):
     """
@@ -17,9 +22,25 @@ def send_email(message):
     an email to your self using 
     smtplib and return 'success ' or 'fail' if sending email failed
     """
+    admin_email = "scelonkululeko2@gmail.com"
+    send_to ="scelonkululeko2@gmail.com"
+    password = "what ever man!"
+    try:
+        with smtplib.SMTP("smtp.gmail.com", 587) as sever:
+            sever.starttls()
+            sever.login(admin_email, password)
+
+            sever.sendmail(from_addr=admin_email, to_addrs=send_to, msg=f"subject: word of the day!\n\n{message}")
+        print("email sent :")
+    except smtplib.SMTPServerDisconnected:
+        print("Error while sending email")
+        print("Connection lost : )")
 
 def main():
     """
     TODO
     write logic to use the two functions here
     """
+    return send_email(get_qoute())
+if __name__ == '__main__':
+    main()
