b2c652c5555f71d49e1d63464c09e17a3d6c1168
THE HONORED ONE! <wtc@pop-os.localdomain>
2024-10-31 15:44:25 +0200

    have code that filters for each table

--- SQL/thief_buster.py
+++ SQL/thief_buster.py
@@ -6,51 +6,148 @@ interview_list = []
 bakery_list = []
 atm_list = []
 flights_list = []
+people_list = []
+airports_list = []
+call_list = []
+bank_account_list = []
+passengers_list = []
+
 conn = sqlite3.connect('SQL/fiftyville.db')
 cur = conn.cursor()
 
 # display data
 crime_scene = cur.execute("SELECT * FROM crime_scene_reports")
-for row in crime_scene:#This gets me the crimes that happened on july 28 2023 at hamphrey street
+for row in crime_scene:
     if "Humphrey Street" in row[4] and 2023 == row[1] and 7 == row[2] and 28 == row[3]:
         crime_scene_list.append(row) 
 
 interviews = cur.execute("SELECT * FROM interviews")
-for row in interviews:#This gets me the crimes that happened on july 28 2023 at hamphrey street
+for row in interviews:
     if 2023 == row[2] and 7 == row[3] and 28 == row[4] :
         interview_list.append(row) 
 
-
 bakery_logs = cur.execute("SELECT * FROM bakery_security_logs")
-for row in bakery_logs:#This gets me the crimes that happened on july 28 2023 at hamphrey street
-    if 2023 == row[1] and 7 == row[2] and 28 == row[3] and 10 == row[4] and 15 == row[5] :
+for row in bakery_logs:
+    if 2023 == row[1] and 7 == row[2] and 28 == row[3] and 10 == row[4] and row[5] <= 25 and row[5] >= 15 :
         bakery_list.append(row)
 
 atm_trans = cur.execute("SELECT * FROM atm_transactions")
-for row in atm_trans:#This gets me the crimes that happened on july 28 2023 at hamphrey street
-    if 2023 == row[2] and 7 == row[3] and 28 == row[4] and "Humphrey Lane" in row[5]:
+for row in atm_trans:
+    if 2023 == row[2] and 7 == row[3] and 28 == row[4] and "Leggett Street" in row[5]:
         atm_list.append(row)
 
-# bank_acc = cur.execute("SELECT * FROM bank_accounts")
-# for row in bank_acc:#This gets me the crimes that happened on july 28 2023 at hamphrey street
-#     # if 2023 == row[2] and 7 == row[3] and 28 == row[4] and "Humphrey Lane" in row[5]:
-#         print(row)
-
-# airports = cur.execute("SELECT * FROM airports")
-# for row in airports:#This gets me the crimes that happened on july 28 2023 at hamphrey street
-#     # if 2023 == row[2] and 7 == row[3] and 28 == row[4] and "Humphrey Lane" in row[5]:
-#         print(row)
+bank_acc = cur.execute("SELECT * FROM bank_accounts")
+for row in bank_acc:
+        bank_account_list.append(row)
 
 flights = cur.execute("SELECT * FROM flights")
-for row in flights:#This gets me the crimes that happened on july 28 2023 at hamphrey street
-    if 2023 == row[3] and 7 == row[4] and 28 == row[5] and row[5] > 10:
+for row in flights:
+    if 2023 == row[3] and 7 == row[4] and 29 == row[5] and row[5] > 10:
         flights_list.append(row)
 
-# calls = cur.execute("SELECT * FROM phone_calls")
-# for row in calls:#This gets me the crimes that happened on july 28 2023 at hamphrey street
-#     if 2023 == row[3] and 7 == row[4] and 28 == row[5]:
-#         print(row)
+calls = cur.execute("SELECT * FROM phone_calls")
+for row in calls:
+    if 2023 == row[3] and 7 == row[4] and 28 == row[5]:
+            call_list.append(row)
+
+people = cur.execute("SELECT * FROM people")
+for row in people:
+      people_list.append(row)
+
+airports = cur.execute("SELECT * FROM airports")
+for row in airports:
+      airports_list.append(row)
+
+passengers = cur.execute("SELECT * FROM passengers")
+for row in passengers:
+      passengers_list.append(row)
 
 #To close connection
 cur.close()
 conn.close()
+
+# print("Crime report")
+# for i in range(len(crime_scene_list)):
+#     if "CS50 duck" in str(crime_scene_list[i]):
+#         print(crime_scene_list[i], "\n*************************************************************************")
+#         print()
+
+# print("Corrisponding invterviews")
+# for i in range(len(interview_list)):
+#     if "bakery" in str(interview_list[i]):
+#         print(interview_list[i], "\n")
+# print("**********************************************************************************\n")
+
+print("Thief's car number plate")
+num_plates = []
+for i in range(len(bakery_list)):# thiefs car number plates
+    bakery_split = str(bakery_list[i]).split(",")
+    if "18" in bakery_split[5]:
+        print(bakery_list[i])
+        num_plates.append(bakery_split[7].replace(")", ""))
+
+print(num_plates)
+print("**********************************************************************************\n")
+
+# print("Widthrawals at Leggett Street ")
+# for i in range(len(atm_list)):
+#     atm_split = str(atm_list[i]).split(",")
+#     if "withdraw" in atm_split[6]:
+#         print(atm_list[i])
+
+# print("flight out of Fiftyville tomorrow(29 july 2023) that the would hav taken")
+# for i in range(len(flights_list)):
+#     flights_split = str(flights_list[i]).split(",")
+#     if  "8" in flights_split[6]: #and "20" in flights_split[7]:
+#         print(flights_list[i])
+# print("**********************************************************************************\n")
+
+# print("The thiefs destination.")
+
+# for i in range(len(airports_list)):
+#     airports_split = str(airports_list[i]).split(",")
+#     if "4" in airports_split[0]:
+#         print(airports_list[i])
+#         print("The thief fled to ", airports_split[3])
+# print("***************************************************")
+
+# print("The thief is... :")
+# thiefs_num = "" 
+# for i in range(len(people_list)):
+#      people_split = str(people_list[i]).split(",")
+#      if num_plates[1] in people_split[4]:
+#           print(people_list[i])
+#           thiefs_num = people_split[2]
+
+# print(thiefs_num)
+# print("******************************************************************")
+
+# print("The call's under 60 minutes")
+# suspects_calls = []
+# for i in range(len(call_list)):
+#      call_split = str(call_list[i]).split(",")
+#      call_split2 = call_split[6].split(")")
+#      if int(call_split2[0]) < 60:
+#         #   print(call_list[i], "\n")
+#           suspects_calls.append(call_list[i])
+
+# # print(suspects_calls)
+# print("**************************************************************")
+# print("Thief's call recever num:")
+# accomplice_num = ""
+# for i in range(len(suspects_calls)):
+#     suspects_calls_split = str(suspects_calls[i]).split(",")
+#     if thiefs_num in suspects_calls_split[1]:
+#         print(suspects_calls[i])
+#         accomplice_num = suspects_calls_split[2]
+# print(accomplice_num)
+
+# print("*************************************************************************")
+# accomplice_name = ""
+# for i in range(len(people_list)):
+#      people_split = str(people_list[i]).split(",")
+#      if accomplice_num in people_split[2]:
+#           print(people_list[i])
+#           accomplice_name = people_split[1]
+
+# print(accomplice_name)
