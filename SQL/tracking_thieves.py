959788c59f6b54846c3fbcfd0cb15ca29140a153
MothoF <mothofeelama@gmail.com>
2024-11-01 22:41:58 +0200

    pinning the thief's flight destination

--- SQL/tracking_thieves.py
+++ SQL/tracking_thieves.py
@@ -31,16 +31,22 @@ def identify_thieves():
 
     #Placing suspect/s at the airport
     print("\n\nThe final suspects are: ")
-    fourth_round_of_suspects = database.execute("SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year='2023' AND month='7' AND day='28' AND hour='10' AND minute BETWEEN '15' AND '25') AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year='2023' AND month='7' AND day='28' AND atm_location='Leggett Street' AND transaction_type='withdraw')) AND phone_number IN (SELECT caller FROM phone_calls WHERE year='2023' AND month='7' AND day='28' AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers) AND (SELECT flight_id from passengers WHERE flight_id IN (SELECT id FROM flights WHERE origin_airport_id IN (SELECT id FROM airports WHERE city='Fiftyville' AND year='2023' AND month='7' AND day='29')))")
-    print(fourth_round_of_suspects)
+    prime_suspects = database.execute("SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year='2023' AND month='7' AND day='28' AND hour='10' AND minute BETWEEN '15' AND '25') AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year='2023' AND month='7' AND day='28' AND atm_location='Leggett Street' AND transaction_type='withdraw')) AND phone_number IN (SELECT caller FROM phone_calls WHERE year='2023' AND month='7' AND day='28' AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers) AND (SELECT flight_id from passengers WHERE flight_id IN (SELECT id FROM flights WHERE origin_airport_id IN (SELECT id FROM airports WHERE city='Fiftyville' AND year='2023' AND month='7' AND day='29')))")
+    print(prime_suspects)
     
     print('\n\nThe flight times from Fiftyville on 29 July 2023 are as follows: ')
     flight_times = database.execute("SELECT hour,minute FROM flights WHERE year='2023' AND month='7' AND day='29' AND origin_airport_id IN (SELECT id FROM airports WHERE city = 'Fiftyville')")
     print(flight_times)
 
-    print('\n\nThe prime suspect is: ')
-    prime_suspect = database.execute("SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year='2023' AND month='7' AND day='28' AND hour='10' AND minute BETWEEN '15' AND '25') AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year='2023' AND month='7' AND day='28' AND atm_location='Leggett Street' AND transaction_type='withdraw')) AND phone_number IN (SELECT caller FROM phone_calls WHERE year='2023' AND month='7' AND day='28' AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers) AND (SELECT flight_id from passengers WHERE flight_id IN (SELECT id FROM flights WHERE hour='8' AND minute='20' AND  origin_airport_id IN (SELECT id FROM airports WHERE city='Fiftyville' AND year='2023' AND month='7' AND day='29'))) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE year='2023' AND month='7' AND day='29' ORDER BY hour ASC LIMIT 1))")
-    print(prime_suspect)
+    #identifying the thief
+    print('\n\nThe thief is: ')
+    thief = database.execute("SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs WHERE year='2023' AND month='7' AND day='28' AND hour='10' AND minute BETWEEN '15' AND '25') AND id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE year='2023' AND month='7' AND day='28' AND atm_location='Leggett Street' AND transaction_type='withdraw')) AND phone_number IN (SELECT caller FROM phone_calls WHERE year='2023' AND month='7' AND day='28' AND duration<60) AND passport_number IN (SELECT passport_number FROM passengers) AND (SELECT flight_id from passengers WHERE flight_id IN (SELECT id FROM flights WHERE hour='8' AND minute='20' AND  origin_airport_id IN (SELECT id FROM airports WHERE city='Fiftyville' AND year='2023' AND month='7' AND day='29'))) AND passport_number IN (SELECT passport_number FROM passengers WHERE flight_id IN (SELECT id FROM flights WHERE year='2023' AND month='7' AND day='29' ORDER BY hour ASC LIMIT 1))")
+    print(thief)
+
+    #finding the thief's destination
+    print("\n\nBruce's flight destination is: ")
+    destination = database.execute("SELECT city FROM airports WHERE id IN (SELECT destination_airport_id FROM flights WHERE year='2023' AND month='7' AND day='29' AND hour='8' AND minute ='20')")
+    print(destination)
 
 
 identify_thieves()
